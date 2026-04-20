e28798dcde486e37c5d4aab877c53acc3ef845f6
Breietta Greenwood <bxg220038@utdallas.edu>
2026-04-20 18:48:18 -0500

    Set up the game loop (initialize, game loop, quit). Similar setup to the digital rain lab.

--- src/snakegame.py
+++ src/snakegame.py
@@ -2,4 +2,25 @@ import pygame
 import random
 import time
 
+def main():
+    # 1. Initialize
+    pygame.init()
+    pygame.display.set_caption('Snake Game')
+    resolution = (800, 600)
+    screen = pygame.display.set_mode(resolution)
+    # 2. Game Loop
+    running = True
+    while running:
+        # Game Logic
 
+        # Event Loop
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill('Black')
+        pygame.display.flip()
+    # 3. Quit
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
