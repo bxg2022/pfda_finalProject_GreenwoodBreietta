3a8fb78634bd92f9fc5166bd1aef62aaef9d741b
Breietta Greenwood <bxg220038@utdallas.edu>
2026-05-01 20:09:42 -0500

    Finished setting up the game loop, added basic parameters and colors, added clock and game speed.

--- src/snakegame.py
+++ src/snakegame.py
@@ -1,24 +1,34 @@
 import pygame
 import random
 import time
+      
 
 def main():
     # 1. Initialize
     pygame.init()
     pygame.display.set_caption('Snake Game')
     resolution = (800, 600)
+    game_speed = 15
+    cell = 20
+    score = 0
+    black = pygame.Color(0, 0, 0)
+    red = pygame.Color(255, 0, 0)
+    green = pygame.Color(0, 255, 0)
     screen = pygame.display.set_mode(resolution)
+    fps = pygame.time.Clock()
+
     # 2. Game Loop
     running = True
     while running:
-        # Game Logic
-
         # Event Loop
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        screen.fill('Black')
+        
+
+        screen.fill(black)
         pygame.display.flip()
+        fps.tick(game_speed)
     # 3. Quit
     pygame.quit()
 
