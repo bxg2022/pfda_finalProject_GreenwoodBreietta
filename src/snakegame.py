3c2914e4db58aae028e1aaf50c854ee2a79a695f
Breietta Greenwood <bxg220038@utdallas.edu>
2026-05-01 21:44:45 -0500

    Added snake movement. WASD keys change the snake's direction. Handles an error so the snake can only move one direction at a time.

--- src/snakegame.py
+++ src/snakegame.py
@@ -19,6 +19,8 @@ def main():
 
     snake_body = [[100,50], [90,50], [80,50], [70,50]]
     snake_pos = snake_body[0]
+    direction = 'RIGHT'
+    change_direction = direction
 
 
     # 2. Game Loop
@@ -28,7 +30,39 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            # Keyboard inputs for snake movement
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w:
+                    change_direction = 'UP'
+                if event.key == pygame.K_s:
+                    change_direction = 'DOWN'
+                if event.key == pygame.K_a:
+                    change_direction = 'LEFT'
+                if event.key == pygame.K_d:
+                    change_direction = 'RIGHT'
+        # Moving the snake
+        if direction == 'UP':
+            snake_pos[1] -= cell
+        if direction == 'DOWN':
+            snake_pos[1] += cell
+        if direction == 'LEFT':
+            snake_pos[0] -= cell
+        if direction == 'RIGHT':
+            snake_pos[0] += cell
+        # Handles error, snake can't move in 2 directions at once
+        if change_direction == 'UP' and direction != 'DOWN':
+            direction = 'UP'
+        if change_direction == 'DOWN' and direction != 'UP':
+            direction = 'DOWN'
+        if change_direction == 'LEFT' and direction != 'RIGHT':
+            direction = 'LEFT'
+        if change_direction == 'RIGHT' and direction != 'LEFT':
+            direction = 'RIGHT'
 
+            
+        snake_body.insert(0, list(snake_pos))
+        snake_body.pop()
+        
         screen.fill(black)
         for segment in snake_body:
             pygame.draw.rect(screen, green, pygame.Rect(
